104539d589ef4fe09502eee4a036bc1a237c065b
James Yen <jameshyen@gmail.com>
2018-03-07 15:56:30 -0800

    wat

--- rest-server/src/components/challenges/challengeSQLHelpers.js
+++ rest-server/src/components/challenges/challengeSQLHelpers.js
@@ -1,9 +1,12 @@
 const escape = require('pg-escape');
 
+// console.log(escape.literal(`console.log('hi');`));
+console.log(escape.literal(`Return 1`));
+
 export const addChallengeHelper = ({ title, content, difficulty }) => {
   return `
     INSERT INTO challenges (title, content, difficulty, rating)
-    VALUES ('${escape.string(title)}', '${escape.string(content)}', ${difficulty}, 0)
+    VALUES (${escape.literal(title)}, ${escape.literal(content)}, ${difficulty}, 0)
     RETURNING id, title, content, difficulty
   `;
 };
@@ -11,7 +14,7 @@ export const addChallengeHelper = ({ title, content, difficulty }) => {
 export const addChallengeHelper2 = (testCase, challenge) => {
   return `
     INSERT INTO testcases (content, challenge_id)
-    VALUES ('${escape.string(testCase)}', '${challenge}')
+    VALUES (${escape.literal(testCase)}, '${challenge}')
     RETURNING id, content, challenge_id
   `;
 };
